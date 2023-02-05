648ee85f97ee83f23513e00b81a061955813b484
Alex Dediu <alex.dediu@outlook.it>
2023-02-05 15:47:28 +0100

    nuovo test.py

--- test.py
+++ test.py
@@ -4,10 +4,12 @@ from web3.contract import Contract, ConciseContract
 from solcx import install_solc
 install_solc(version='latest')
 from solcx import compile_source
+import asyncio
 
 def interact_with_smart_contract():
     # Connect to local blockchain using Web3
     w3 = Web3(web3.HTTPProvider('http://127.0.0.1:22000'))
+
 
     # Check if connection is successful
     if w3.isConnected():
@@ -55,8 +57,25 @@ def interact_with_smart_contract():
 
     # Call a method of the contract
     concise = ConciseContract(contract)
-    result = contract.functions.get().call()
+    result = concise.get()
     print("Address of contract method call:", result)
+
+    blocks_number = w3.eth.getTransactionCount(w3.eth.accounts[0]) # get number of blocks of node (of blockchain)
+    print(blocks_number)
+
+    print(w3.eth.accounts[0])
+    
+    new_transaction = {
+        "from": w3.eth.accounts[0],
+        "to": contract_address,
+        "data": contract.encodeABI(fn_name="set", args=[50]),
+        "gas": w3.toHex(3737050400),
+        "gasPrice": '0',
+        "nonce": blocks_number
+    }
+    signedTransaction = w3.eth.signTransaction(new_transaction)
+    sentTransaction = w3.eth.sendRawTransaction(signedTransaction.raw)
+    print(sentTransaction)
 
 # Call the function to interact with the smart contract
 interact_with_smart_contract()
