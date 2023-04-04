60083eb0040dd991fcaa1b9f14155d7efc0371d9
LorenzoLongarini <s1089145@studenti.univpm.it>
2023-04-04 15:46:17 +0200

    conflict solved

--- src/offchain.py
+++ src/offchain.py
@@ -76,15 +76,15 @@ def deploy(w3: Web3, contract: Contract, name: str):
     sm_transaction = {
         "from": w3.eth.accounts[0],
         "gas": w3.to_hex(6721975),
-        "gasPrice": w3.toWei('0', 'gwei'),
+        "gasPrice": w3.to_wei('0', 'gwei'),
         "nonce": w3.eth.get_transaction_count(w3.eth.accounts[0]),
         "data": contract.bytecode
     }
-    signedTransaction = w3.eth.account.signTransaction(
+    signedTransaction = w3.eth.account.sign_transaction(
         sm_transaction, "0x4f11e05b6908439852b5ea7c97da15738dfadd111b3fc89d4c812423fa929b45")
-    transaction_hash = w3.eth.sendRawTransaction(
+    transaction_hash = w3.eth.send_raw_transaction(
         signedTransaction.rawTransaction)
-    tx_receipt = w3.eth.waitForTransactionReceipt(transaction_hash)
+    tx_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
     contract = w3.eth.contract(
         address=tx_receipt.contractAddress, abi=contract.abi, bytecode=contract.bytecode)
     new_map = {
@@ -116,14 +116,14 @@ def write(w3: Web3, contract: Contract, function_name: str, args: any):
         "to": contract.address,
         "data": contract.encodeABI(fn_name=function_name, args=args),
         "gas": w3.to_hex(6721975),
-        "gasPrice": w3.toWei('0', 'gwei'),
+        "gasPrice": w3.to_wei('0', 'gwei'),
         "nonce": w3.eth.get_transaction_count(w3.eth.accounts[0])
     }
-    signedTransaction = w3.eth.account.signTransaction(
+    signedTransaction = w3.eth.account.sign_transaction(
         new_transaction, "0x4f11e05b6908439852b5ea7c97da15738dfadd111b3fc89d4c812423fa929b45")
-    transaction_hash = w3.eth.sendRawTransaction(
+    transaction_hash = w3.eth.send_raw_transaction(
         signedTransaction.rawTransaction)
-    receipt = w3.eth.waitForTransactionReceipt(transaction_hash)
+    receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
     return receipt
 
 
@@ -348,10 +348,10 @@ class SolidityPage(tk.Toplevel):
             smartContractBytecode = smartContractInterface['bin']
             smartContractAbi = smartContractInterface['abi']
             receipt = write(web3_1, onChainSmartContract, "getNextAddress", [])
-            logs = onChainSmartContract.events.NextAddressReturned().processReceipt(receipt)
+            logs = onChainSmartContract.events.NextAddressReturned().process_receipt(receipt)
             nextAddress = logs[0]['args']['nextAddress']
             web3_c = Web3(HTTPProvider(nextAddress))
-            if web3_c.isConnected():
+            if web3_c.is_connected():
                 print("Connected to " + nextAddress)
                 customSmartContract = web3_c.eth.contract(
                     abi=smartContractAbi, bytecode=smartContractBytecode)
@@ -411,7 +411,7 @@ class ABIBytecodePage(tk.Toplevel):
         logs = onChainSmartContract.events.NextAddressReturned().processReceipt(receipt)
         nextAddress = logs[0]['args']['nextAddress']
         web3_c = Web3(HTTPProvider(nextAddress))
-        if web3_c.isConnected():
+        if web3_c.is_connected():
             print("Connected to " + nextAddress)
             customSmartContract = web3_c.eth.contract(
                 abi=smartContractAbi, bytecode=smartContractBytecode)
