df1b1c5fbbe97e78f379dcd17a5384c0303fff29
Massimiliano Piccinini <massimilianopiccinini.10@gmail.com>
2023-04-28 20:10:32 +0200

    Deletion of a Smart Contract

--- src/offchain.py
+++ src/offchain.py
@@ -49,10 +49,12 @@ def read_storage(name: str):
 def deploy(w3: Web3, contract: Contract, name: str):
     sm_transaction = {
         "from": w3.eth.accounts[0],
-        "gas": w3.to_hex(6721975),
-        "gasPrice": w3.to_wei('0', 'gwei'),
+        "maxFeePerGas": w3.to_hex(1000000000000),
+        "maxPriorityFeePerGas": w3.to_hex(0),
+        "gas": w3.to_hex(1000000000000),
         "nonce": w3.eth.get_transaction_count(w3.eth.accounts[0]),
-        "data": contract.bytecode
+        "data": contract.bytecode,
+        'chainId': w3.eth.chain_id
     }
     signedTransaction = w3.eth.account.sign_transaction(sm_transaction, "0x4f11e05b6908439852b5ea7c97da15738dfadd111b3fc89d4c812423fa929b45")
     transaction_hash = w3.eth.send_raw_transaction(signedTransaction.rawTransaction)
@@ -83,10 +85,14 @@ def write(w3: Web3, contract: Contract, function_name: str, args: any):
         "from": w3.eth.accounts[0],
         "to": contract.address,
         "data": contract.encodeABI(fn_name=function_name, args=args),
-        "gas": w3.to_hex(6721975),
-        "gasPrice": w3.to_wei('0', 'gwei'),
-        "nonce": w3.eth.get_transaction_count(w3.eth.accounts[0])
+        "gas": w3.to_hex(1000000000000),
+        # "gasPrice": w3.to_wei('0', 'gwei'),
+        "maxFeePerGas": w3.to_hex(1000000000000),
+        "maxPriorityFeePerGas": w3.to_wei(0, 'gwei'),
+        "nonce": w3.eth.get_transaction_count(w3.eth.accounts[0]),
+        'chainId': w3.eth.chain_id
     }
+    #"gas": w3.to_hex(6721975),
     signedTransaction = w3.eth.account.sign_transaction(new_transaction, "0x4f11e05b6908439852b5ea7c97da15738dfadd111b3fc89d4c812423fa929b45")
     transaction_hash = w3.eth.send_raw_transaction(signedTransaction.rawTransaction)
     receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
@@ -171,22 +177,25 @@ class Loader(tk.Frame):
 class HomePage(tk.Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
-        screen_width = root.winfo_screenwidth()
-        screen_height = root.winfo_screenheight()
-        self.geometry(f"{screen_width//2}x{screen_height//2}+{screen_width//4}+{screen_height//4}")
+        # screen_width = root.winfo_screenwidth()
+        # screen_height = root.winfo_screenheight()
+        # self.geometry(f"{screen_width//2}x{screen_height//2}+{screen_width//4}+{screen_height//4}")
         self.title("Home Page")
+
         current_dir = os.path.dirname(os.path.abspath(__file__))
         button1_image = Image.open(os.path.join(current_dir, "receipt_long.png"))
         button2_image = Image.open(os.path.join(current_dir, "assignment.png"))
         button3_image = Image.open(os.path.join(current_dir, "tty.png"))
+        button4_image = Image.open(os.path.join(current_dir, "delete.png"))
         button1_photo = ImageTk.PhotoImage(button1_image)
         button2_photo = ImageTk.PhotoImage(button2_image)
         button3_photo = ImageTk.PhotoImage(button3_image)
+        button4_photo = ImageTk.PhotoImage(button4_image)
 
         title_label = tk.Label(self, text="Scegli come deployare il tuo smart contract, oppure se richiamare un metodo di uno smart contract esistente.", font=("Arial", 13))
         title_label.grid(row=0, column=0, columnspan=3, pady=20)
 
-        button1 = tk.Button(self, image=button1_photo, text="Deploy file .sol", compound=tk.TOP, font=("Arial", 12), command=self.button1_clicked)
+        button1 = tk.Button(self, image=button1_photo, text="Deploy il file .sol", compound=tk.TOP, font=("Arial", 12), command=self.button1_clicked)
         button1.image = button1_photo
         button1.grid(row=1, column=0, padx=0, pady=10)
         frame1 = tk.Frame(self, height=100, width=100)
@@ -213,6 +222,15 @@ class HomePage(tk.Toplevel):
         label3 = tk.Label(frame3, text="Chiama un\nmetodo di uno\nSmart Contract\nesistente", font=("Arial", 13))
         label3.pack(fill=tk.BOTH, expand=1)
 
+        button4 = tk.Button(self, image=button4_photo, text="Elimina Smart Contract", compound=tk.TOP, font=("Arial", 12), command=self.button4_clicked)
+        button4.image = button4_photo
+        button4.grid(row=1, column=3, padx=0, pady=10)
+        frame4 = tk.Frame(self, height=100, width=100)
+        frame4.pack_propagate(0)
+        frame4.grid(row=2, column=3, padx=0, pady=10)
+        label4 = tk.Label(frame4, text="Elimina uno\nSmart Contract", font=("Arial", 13))
+        label4.pack(fill=tk.BOTH, expand=1)
+
     def get_folder_path():
         while True:
             folder_path = input("Please enter the path of the folder: ")
@@ -230,14 +248,17 @@ class HomePage(tk.Toplevel):
     def button3_clicked(self):
         callmethodpage = MethodsPage(self.master)
 
+    def button4_clicked(self):
+        deletepage = DeletePage(self.master)
+
 class SolidityPage(tk.Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
-        screen_width = root.winfo_screenwidth()
-        screen_height = root.winfo_screenheight()
-        self.geometry(f"{screen_width//2}x{screen_height//2}+{screen_width//4}+{screen_height//4}")
+        # screen_width = root.winfo_screenwidth()
+        # screen_height = root.winfo_screenheight()
+        # self.geometry(f"{screen_width//2}x{screen_height//2}+{screen_width//4}+{screen_height//4}")
         self.title("Deploy file .sol")
-        self.name_label = tk.Label(self, text="Name:")
+        self.name_label = tk.Label(self, text="Nome:")
         self.name_label.pack()
         
         self.name_entry = tk.Entry(self)
@@ -249,14 +270,18 @@ class SolidityPage(tk.Toplevel):
         self.file_entry = tk.Entry(self, state="readonly")
         self.file_entry.pack()
         
-        self.browse_button = tk.Button(self, text="Browse", command=self.browse_file)
+        self.browse_button = tk.Button(self, text="Cerca", command=self.browse_file)
         self.browse_button.pack()
+
+        frame = tk.Frame(self)
         
-        self.ok_button = tk.Button(self, text="OK", command=self.ok_button_click)
+        self.ok_button = tk.Button(frame, text="Deploy", command=self.ok_button_click)
         self.ok_button.pack(side=tk.LEFT)
         
-        self.cancel_button = tk.Button(self, text="Cancel", command=self.cancel_button_click)
+        self.cancel_button = tk.Button(frame, text="Annulla", command=self.cancel_button_click)
         self.cancel_button.pack(side=tk.LEFT)
+
+        frame.pack()
 
     def browse_file(self):
         filetypes = (("Solidity files", "*.sol"), ("All files", "*.*"))
@@ -279,6 +304,9 @@ class SolidityPage(tk.Toplevel):
             smartContractAbi = smartContractInterface['abi']
             receipt = write(web3_1, onChainSmartContract, "getNextAddress", [])
             logs = onChainSmartContract.events.NextAddressReturned().process_receipt(receipt)
+            print(receipt)
+            print(logs)
+            print(logs[0])
             nextAddress = logs[0]['args']['nextAddress']
             web3_c = Web3(HTTPProvider(nextAddress))
             if web3_c.is_connected():
@@ -288,6 +316,7 @@ class SolidityPage(tk.Toplevel):
                 write(web3_1, onChainSmartContract, 'addContract', [name, str(nextAddress), str(customSmartContract.address), str(smartContractAbi)])
                 result = read(onChainSmartContract, 'getContract', [name])
                 print("Result: " + str(result))
+                show_toast('Lo Smart Contract è stato deployato con successo', '')
             else:
                 print("Not connected to " + nextAddress)
 
@@ -299,11 +328,11 @@ class SolidityPage(tk.Toplevel):
 class ABIBytecodePage(tk.Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
-        screen_width = root.winfo_screenwidth()
-        screen_height = root.winfo_screenheight()
-        self.geometry(f"{screen_width//2}x{screen_height//2}+{screen_width//4}+{screen_height//4}")
+        # screen_width = root.winfo_screenwidth()
+        # screen_height = root.winfo_screenheight()
+        # self.geometry(f"{screen_width//2}x{screen_height//2}+{screen_width//4}+{screen_height//4}")
         self.title("Deploy da ABI e Bytecode")
-        self.name_label = tk.Label(self, text="Name:")
+        self.name_label = tk.Label(self, text="Nome:")
         self.name_label.pack()
         
         self.name_entry = tk.Entry(self)
@@ -320,12 +349,16 @@ class ABIBytecodePage(tk.Toplevel):
         
         self.bytecode_label = tk.Entry(self)
         self.bytecode_label.pack()
+
+        frame = tk.Frame(self)
         
-        self.ok_button = tk.Button(self, text="OK", command=self.ok_button_click)
+        self.ok_button = tk.Button(frame, text="Deploy", command=self.ok_button_click)
         self.ok_button.pack(side=tk.LEFT)
         
-        self.cancel_button = tk.Button(self, text="Cancel", command=self.cancel_button_click)
+        self.cancel_button = tk.Button(frame, text="Annulla", command=self.cancel_button_click)
         self.cancel_button.pack(side=tk.LEFT)
+
+        frame.pack()
         
     def ok_button_click(self):
         name = self.name_entry.get()
@@ -342,6 +375,7 @@ class ABIBytecodePage(tk.Toplevel):
             write(web3_1, onChainSmartContract, 'addContract', [name, str(nextAddress), str(customSmartContract.address), str(smartContractAbi)])
             result = read(onChainSmartContract, 'getContract', [name])
             print("Result: " + str(result))
+            show_toast('Lo Smart Contract è stato deployato con successo', '')
         else:
             print("Not connected to " + nextAddress)
 
@@ -353,17 +387,17 @@ class ABIBytecodePage(tk.Toplevel):
 class MethodsPage(tk.Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
-        screen_width = root.winfo_screenwidth()
-        screen_height = root.winfo_screenheight()
-        self.geometry(f"{screen_width//2}x{screen_height//2}+{screen_width//4}+{screen_height//4}")
+        # screen_width = root.winfo_screenwidth()
+        # screen_height = root.winfo_screenheight()
+        # self.geometry(f"{screen_width//2}x{screen_height//2}+{screen_width//4}+{screen_height//4}")
         self.title("Chiama metodo")
-        self.name_label = tk.Label(self, text="Name of the smart contract:")
+        self.name_label = tk.Label(self, text="Nome dello Smart Contract:")
         self.name_label.pack()
         
         self.name_entry = tk.Entry(self)
         self.name_entry.pack()
         
-        self.function_label = tk.Label(self, text="Function name:")
+        self.function_label = tk.Label(self, text="Nome della Funzione:")
         self.function_label.pack()
         
         self.function_label = tk.Entry(self)
@@ -394,12 +428,16 @@ class MethodsPage(tk.Toplevel):
         
         self.arg3_label = tk.Entry(self)
         self.arg3_label.pack()
+
+        frame = tk.Frame(self)
         
-        self.ok_button = tk.Button(self, text="OK", command=self.ok_button_click)
+        self.ok_button = tk.Button(frame, text="OK", command=self.ok_button_click)
         self.ok_button.pack(side=tk.LEFT)
         
-        self.cancel_button = tk.Button(self, text="Cancel", command=self.cancel_button_click)
+        self.cancel_button = tk.Button(frame, text="Annulla", command=self.cancel_button_click)
         self.cancel_button.pack(side=tk.LEFT)
+
+        frame.pack()
         
     def ok_button_click(self):
         selected_option = self.selected_option.get()
@@ -436,42 +474,75 @@ class MethodsPage(tk.Toplevel):
                     result = read(customSmartContract, function, [])
                 else:
                     write(web3_c, customSmartContract, function, [])
+            if selected_option == 0:
+                show_toast('Dati ottenuti correttamente', str(result))
+            else:
+                show_toast('Dati scritti correttamente', '')
+            self.destroy()
         else:
+            show_toast('Lo Smart Contract non esiste', 'Deployalo prima di eseguire un suo metodo')
             print("Not connected to " + blockChainAddress)
-        if selected_option == 0:
-            self.show_toast('Dati ottenuti correttamente', str(result))
-        else:
-            self.show_toast('Dati scritti correttamente', '')
-        self.destroy()
         
     def cancel_button_click(self):
         self.destroy()
 
-    def show_toast(self, title, description):
-        window = tk.Toplevel()
-        window.overrideredirect(True)
-        window.attributes("-topmost", True)
-        screen_width = root.winfo_screenwidth()
-        screen_height = root.winfo_screenheight()
-        window.geometry(f"{500}x{100}+{screen_width//2-50}+{screen_height//2-50}")
+def show_toast(title, description):
+    window = tk.Toplevel()
+    window.overrideredirect(True)
+    window.attributes("-topmost", True)
+    # screen_width = root.winfo_screenwidth()
+    # screen_height = root.winfo_screenheight()
+    # window.geometry(f"{500}x{100}+{screen_width//2-50}+{screen_height//2-50}")
 
-        # create a frame for the toast message
-        frame = tk.Frame(window, bg='white', bd=1, relief=tk.RAISED)
-        frame.pack(side=tk.BOTTOM, padx=10, pady=10)
+    # create a frame for the toast message
+    frame = tk.Frame(window, bg='white', bd=1, relief=tk.RAISED)
+    frame.pack(side=tk.BOTTOM, padx=10, pady=10)
 
-        # create a label for the title and add it to the frame
-        title_label = tk.Label(frame, text=title, font=('Arial', 14, 'bold'), fg='black', bg='white')
-        title_label.pack(padx=10, pady=5)
+    # create a label for the title and add it to the frame
+    title_label = tk.Label(frame, text=title, font=('Arial', 14, 'bold'), fg='black', bg='white')
+    title_label.pack(padx=10, pady=5)
 
-        # create a label for the description and add it to the frame
-        desc_label = tk.Label(frame, text=description, font=('Arial', 12), fg='gray', bg='white')
-        desc_label.pack(padx=10, pady=5)
+    # create a label for the description and add it to the frame
+    desc_label = tk.Label(frame, text=description, font=('Arial', 12), fg='gray', bg='white')
+    desc_label.pack(padx=10, pady=5)
 
-        # function to destroy the window after a short delay
-        def destroy_window():
-            window.after(3000, window.destroy)
+    # function to destroy the window after a short delay
+    def destroy_window():
+        window.after(3000, window.destroy)
+    
+    window.after(3000, destroy_window)
+
+class DeletePage(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        # screen_width = root.winfo_screenwidth()
+        # screen_height = root.winfo_screenheight()
+        # self.geometry(f"{screen_width//2}x{screen_height//2}+{screen_width//4}+{screen_height//4}")
+        self.title("Elimina Smart Contract")
+        self.name_label = tk.Label(self, text="Nome:")
+        self.name_label.pack()
         
-        window.after(3000, destroy_window)
+        self.name_entry = tk.Entry(self)
+        self.name_entry.pack()
+
+        frame = tk.Frame(self)
+        
+        self.ok_button = tk.Button(frame, text="Elimina", command=self.ok_button_click)
+        self.ok_button.pack(side=tk.LEFT)
+        
+        self.cancel_button = tk.Button(frame, text="Annulla", command=self.cancel_button_click)
+        self.cancel_button.pack(side=tk.LEFT)
+
+        frame.pack()
+        
+    def ok_button_click(self):
+        name = self.name_entry.get()
+        write(web3_1, onChainSmartContract, "deleteContract", [name])
+        show_toast('Smart Contract eliminato correttamente', '')
+        self.destroy()
+        
+    def cancel_button_click(self):
+        self.destroy()
 
 root = tk.Tk()
 app = Loader(root)
